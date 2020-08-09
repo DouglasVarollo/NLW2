56aa19e7128afe7d54aab85dc8a24722d174299d
Douglas Varollo <dcvarollo@gmail.com>
2020-08-09 14:50:22 -0300

    add user registration

--- server/src/database/migrations/20200804190854_create_users.ts
+++ server/src/database/migrations/20200804190854_create_users.ts
@@ -1,15 +1,21 @@
 import Knex from "knex";
 
 export async function up(knex: Knex): Promise<void> {
-	return knex.schema.createTable("users", (table) => {
-		table.increments("id").primary();
-		table.string("name").notNullable();
-		table.string("avatar").notNullable();
-		table.string("whatsapp").notNullable();
-		table.string("bio").notNullable();
-	});
+  return knex.schema.createTable("users", (table) => {
+    table.increments("id").primary();
+    table.string("first_name").notNullable();
+    table.string("last_name").notNullable();
+    table.string("email").notNullable();
+    table.string("password_hash").notNullable();
+    table
+      .string("avatar_url")
+      .notNullable()
+      .defaultTo("https://api.adorable.io/avatars/285/abott@adorable.png");
+    table.string("whatsapp").notNullable().defaultTo("");
+    table.string("bio").notNullable().defaultTo("");
+  });
 }
 
 export async function down(knex: Knex): Promise<void> {
-	return knex.schema.dropTable("users");
+  return knex.schema.dropTable("users");
 }
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -2,15 +2,19 @@ import { Router } from "express";
 
 import ClassesController from "./controllers/ClassesController";
 import ConnectionsController from "./controllers/ConnectionsController";
+import UsersController from "./controllers/UsersController";
 
 const routes = Router();
 const classesController = new ClassesController();
 const connectionsController = new ConnectionsController();
+const usersController = new UsersController();
 
 routes.get("/classes", classesController.index);
 routes.post("/classes", classesController.create);
 
 routes.get("/connections", connectionsController.index);
 routes.post("/connections", connectionsController.create);
+
+routes.post("/users", usersController.create);
 
 export default routes;
